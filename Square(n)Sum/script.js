90c613323ad8f433e337c50f22858fbe355b3559
SergeyKardashev <angeldeep@yandex.ru>
2024-03-23 22:18:03 +0700

    new structure and simple kata improvement

---
+++ Square(n)Sum/script.js
@@ -0,0 +1,27 @@
+// Square(n)Sum
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//
+//
+// iterating through the array using the forEach method,
+// storing the value in a variable in the scope of the function
+function squareSum_01(numbers) {
+  let result = 0;
+  numbers.forEach((i) => {
+    result = result + i * i;
+  });
+
+  return result;
+}
+
+console.log("squareSum_01", squareSum_01([1, 2, 2]));
+//
+//
+// the same with +=
+function squareSum_02(numbers) {
+  let result = 0;
+  numbers.forEach((i) => {
+    result += i * i;
+  });
+
+  return result;
+}
