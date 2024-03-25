7f54e1b12020b5eb415d76783355f96b9018430b
SergeyKardashev <angeldeep@yandex.ru>
2024-03-25 18:30:27 +0700

    reduce() with short increment += and with **2

--- Square(n)Sum/script.js
+++ Square(n)Sum/script.js
@@ -16,7 +16,7 @@ function squareSum_01(numbers) {
 console.log("squareSum_01", squareSum_01([1, 2, 2]));
 //
 //
-// the same with +=
+// the same with short increment +=
 function squareSum_02(numbers) {
   let result = 0;
   numbers.forEach((i) => {
@@ -27,9 +27,29 @@ function squareSum_02(numbers) {
 }
 //
 //
-//
+// reduce()
 function squareSum_03(numbers) {
   return numbers.reduce((acc, num) => acc + num * num, 0);
 }
 
 console.log("squareSum_03", squareSum_03([1, 2, 2]));
+
+// pay attention to initial value
+// console.log(
+//   "reduce, WITHOUT initial value, [10, 5]",
+//   [10, 5].reduce((acc, num) => acc + num * num)
+// );
+
+// console.log(
+//   "reduce, initial value is 0, [10, 5]",
+//   [10, 5].reduce((acc, num) => acc + num * num, 0)
+// );
+//
+//
+//
+// reduce() with short increment += and with **2
+function squareSum_04(numbers) {
+  return numbers.reduce((acc, num) => (acc += num ** 2), 0);
+}
+
+console.log("squareSum_04", squareSum_04([1, 2, 2]));
