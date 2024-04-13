0ed9c4ba428750f158c30edfb48dc5d2153a6ec6
SergeyKardashev <angeldeep@yandex.ru>
2024-04-13 16:19:20 +0700

    Fast cooking pancakes solved. Two solutions

--- Fast_cooking_pancakes/script.js
+++ Fast_cooking_pancakes/script.js
@@ -1,36 +1,30 @@
 // Fast cooking pancakes
-// You need to cook pancakes, but you are very hungry. 
+// You need to cook pancakes, but you are very hungry.
 // As known, one needs to fry a pancake one minute on each side.
-// 
-// What is the minimum time you need to cook n pancakes, 
+//
+// What is the minimum time you need to cook n pancakes,
 // if you can put on the frying pan only m pancakes at a time ?
-// 
+//
 // n and m are positive integers between 1 and 1000.
-//
-//
-//
-// function cookPancakes(n, m) {
-//    some code
-// }
 
-function cookPancakes(cakes, ovenSize) {
-  console.log(cakes, ovenSize);
-  if (cakes <= ovenSize) {
+function cookPancakes(cakes, slots) {
+  if (cakes <= slots) {
+    console.log("cakes < or = slots", "cakes", cakes, "slots", slots, "res 2");
     return 2;
   }
-  if (cakes % ovenSize === 0) {
-    return cakes / ovenSize * 2;
-  }
-  return (Math.floor(cakes / ovenSize) + 1) * 2 - 1;
+  
+  // 3 cakes and 2 slots result 3, 4 cakes and 3 slots result 3
+  // There is the quotient of integer division in the last line return (Math.floor)
+  // 8 cakes = 16 sides
+  if ((cakes * 2) % slots !== 0) {
+    let res = Math.floor(cakes / slots * 2) + 1;
+    console.log("% != 0", "cakes", cakes, "slots", slots, "res", res);
+    return res;
+  } 
+
+  return cakes / slots * 2;
 }
 
-console.log(cookPancakes(1, 2));
-console.log(cookPancakes(2, 2));
-console.log(cookPancakes(3, 2));
-console.log(cookPancakes(617, 411));
-console.log(cookPancakes(222, 65));
-
-// console.log(cookPancakes(8, 3));
-// There is the quotient of integer division in the last line return (Math.floor)
-// 8 cakes = 16 sides
-// 
+function cookPancakes(cakes, slots) {
+    return cakes < slots ? 2 : Math.ceil(cakes / slots * 2);
+  }
