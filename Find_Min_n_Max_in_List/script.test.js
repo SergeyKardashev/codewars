ab82542ebe712c8376f75e3742875fabb7ca3e62
SergeyKardashev <angeldeep@yandex.ru>
2024-06-11 14:31:30 +0700

    feat: 1) add test cases to min max functions; 2) rename functions

--- Find_Min_n_Max_in_List/script.test.js
+++ Find_Min_n_Max_in_List/script.test.js
@@ -1,19 +1,23 @@
-const { min1, max1 } = require('./script');
+const { min1: min, max1: max } = require('./script');
 
 describe('Functions min and max should return min and max values', () => {
     test('case #1', () => {
-        expect(min1([4,6,2,1,9,63,-134,566])).toBe(-134);
-        expect(max1([4,6,2,1,9,63,-134,566])).toBe(566);
+        expect(min([4,6,2,1,9,63,-134,566])).toBe(-134);
+        expect(max([4,6,2,1,9,63,-134,566])).toBe(566);
     });
 
     test('case #2', () => {
-        expect(min1([-52, 56, 30, 29, -54, 0, -110])).toBe(-110);
-        expect(max1([-52, 56, 30, 29, -54, 0, -110])).toBe(56);
+        expect(min([-52, 56, 30, 29, -54, 0, -110])).toBe(-110);
+        expect(max([-52, 56, 30, 29, -54, 0, -110])).toBe(56);
+    });
+
+    test('case #2', () => {
+        expect(min([42, 54, 65, 87, 0])).toBe(0);
+        expect(max([42, 54, 65, 87, 0])).toBe(87);
+    });
+
+    test('case #2', () => {
+        expect(min([5])).toBe(5);
+        expect(max([5])).toBe(5);
     });
 });
-
-// Examples(Input -> Output)
-// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
-// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
-// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
-// * [5]   
