65c1bdba658d1a100821e7fae16f59c7cc040cc4
SergeyKardashev <angeldeep@yandex.ru>
2024-08-29 18:24:30 +0700

    feat: add group to Grasshopper test

--- Grasshopper-Terminal_game_move_function/script.test.js
+++ Grasshopper-Terminal_game_move_function/script.test.js
@@ -2,6 +2,8 @@
 
 const move = require('./script');
 
-test('Function move should return number according to the task', () => {
-  expect(move(3, 6)).toBe(15);
+describe('', () => {
+  test('Function move should return number according to the task', () => {
+    expect(move(3, 6)).toBe(15);
+  });
 });
